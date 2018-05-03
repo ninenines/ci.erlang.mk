c87ea23cf8f657e6c630c46b08fac52e26113506
Loïc Hoguin <essen@ninenines.eu>
2018-05-03 12:53:47 +0200

    Add 20.3 to Windows

--- early-plugins.mk
+++ early-plugins.mk
@@ -76,7 +76,7 @@ ifeq ($(PLATFORM),msys2)
 
 WINDOWS-OTP-18 := 18.0 18.1 18.2.1 18.3
 WINDOWS-OTP-19 := 19.0 19.1 19.2 19.3
-WINDOWS-OTP-20 := 20.0 20.1 20.2
+WINDOWS-OTP-20 := 20.0 20.1 20.2 20.3
 
 WINDOWS-OTP-18+ := $(WINDOWS-OTP-18) $(WINDOWS-OTP-19) $(WINDOWS-OTP-20)
 WINDOWS-OTP-19+ := $(WINDOWS-OTP-19) $(WINDOWS-OTP-20)
@@ -99,6 +99,7 @@ WINDOWS-OTP-19.3-INSTALL-DIR := $(call msys2_path,$(PROGRAMFILES)/erl8.3/bin)
 WINDOWS-OTP-20.0-INSTALL-DIR := $(call msys2_path,$(PROGRAMFILES)/erl9.0/bin)
 WINDOWS-OTP-20.1-INSTALL-DIR := $(call msys2_path,$(PROGRAMFILES)/erl9.1/bin)
 WINDOWS-OTP-20.2-INSTALL-DIR := $(call msys2_path,$(PROGRAMFILES)/erl9.2/bin)
+WINDOWS-OTP-20.3-INSTALL-DIR := $(call msys2_path,$(PROGRAMFILES)/erl9.3/bin)
 
 # We have to duplicate this for it to work as
 # it is not yet defined when we define this.
